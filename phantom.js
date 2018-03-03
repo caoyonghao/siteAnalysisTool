b9db708718022695bbee21c77101f06e3bd985e6
caoyonghao <caoyonghao@caoyonghaodeMacBook-Air.local>
2018-03-04 00:20:21 +0800

    test

--- phantom.js
+++ phantom.js
@@ -1,7 +1,9 @@
 var page = require('webpage').create();
+var count = 0;
 page.open('http://www.huaweicloud.com', function() {
     page.evaluate(function() {
-      console.log($('a').length);
+      count = $('a').length;
     });
+    console.log(count);
     phantom.exit();
 });
