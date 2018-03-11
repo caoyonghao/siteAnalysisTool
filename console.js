50ddfd3bba15719f63dba1b2cf776829b1383716
caoyonghao <caoyonghao@caoyonghaodeMacBook-Air.local>
2018-03-11 17:20:09 +0800

    调整目录结构

--- console.js
+++ console.js
@@ -1,16 +1,18 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
-const sendMail = require('./mail/mailServer');
+const sendMail = require('./lib/mail/mailServer');
 
 const timeStamp = new Date().getTime();
-const crawler = spawn('node', ['crawler.js', 'http://www.huaweicloud.com/', timeStamp]);
+const crawler = spawn('node', ['./lib/crawler/crawler.js', 'http://www.huaweicloud.com/', timeStamp]);
 let log = '';
 crawler.stdout.on('data', (data) => {
-  log = `${log}${new Date()} INFO:${data}`;
+    log = `${log}${new Date()} INFO:${data}`;
+    console.log(data.toString());
 });
 
 crawler.stderr.on('data', (data) => {
     log = `${log}${new Date()} ERROR:${data}`;
+    console.log(data.toString());
 });
 
 crawler.on('close', (code) => {
--- test.js
+++
@@ -1,6 +0,0 @@
-var $ = require("./jq").jq();
- 
-$.get("http://www.aliyun.com", "gbk", function (html) {
-    var title = html.find("title").text();
-    console.log(html);
-});
