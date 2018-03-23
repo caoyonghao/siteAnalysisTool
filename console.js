1eccaa333144fe01a3b40b52d543ece373acd4b7
caoyonghao <491476941@qq.com>
2018-03-23 09:43:47 +0800

    add maillist

--- console.js
+++ console.js
@@ -22,7 +22,7 @@ const exec = () => {
     crawler.on('close', (code) => {
         fs.writeFileSync('./log.log', log);
         if (options.sendMail) {
-            sendMail('yonghao.cao@huawei.com', `-${new Date().getMonth() + 1}/${new Date().getDate()}`, generateReport(require(`./result/${timeStamp}-fail.json`)), [
+            sendMail('yonghao.cao@huawei.com,huoxiangming@huawei.com,yangzhao15@huawei.com,wanglong42@huawei.com,yangzhongting@huawei.com,shengzhong@huawei.com', `-${new Date().getMonth() + 1}/${new Date().getDate()}`, generateReport(require(`./result/${timeStamp}-fail.json`)), [
                 {
                     filename: `${timeStamp}-fail.json`,
                     path: `./result/${timeStamp}-fail.json`
