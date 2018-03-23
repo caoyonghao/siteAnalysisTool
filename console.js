27692bf5cdbbe1a7cc3bde473367891d0276e09c
caoyonghao <491476941@qq.com>
2018-03-23 10:22:10 +0800

    统一时间标志

--- console.js
+++ console.js
@@ -6,8 +6,31 @@ const generateReport = require('./lib/mail/generateReport');
 const rule = new schedule.RecurrenceRule();
 let options = {};
 
+Date.prototype.format = function(format) {
+
+    var date = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S+": this.getMilliseconds()
+    };
+    if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+        }
+    }
+    return format;
+}
+
 const exec = () => {
-    const timeStamp = new Date().getTime();
+    const timeStamp = new Date().getTime().format('yyyyMMdd_hh:mm:ss');
     const crawler = spawn('node', ['./lib/crawler/crawler.js', 'http://www.huaweicloud.com/', timeStamp]);
     let log = '';
     crawler.stdout.on('data', (data) => {
@@ -22,16 +45,16 @@ const exec = () => {
     crawler.on('close', (code) => {
         fs.writeFileSync('./log.log', log);
         if (options.sendMail) {
-            const failList = require(`./result/${timeStamp}-fail.json`);
+            const failList = require(`./result/fail-${timeStamp}.json`);
             const length = Object.keys(failList).length;
             if (length) {
                 sendMail('yonghao.cao@huawei.com,huoxiangming@huawei.com,yangzhao15@huawei.com,wanglong42@huawei.com,yangzhongting@huawei.com,shengzhong@huawei.com', `-${new Date().getMonth() + 1}/${new Date().getDate()}`, generateReport(require(`./result/${timeStamp}-fail.json`)), [
                     {
-                        filename: `${timeStamp}-fail.json`,
-                        path: `./result/${timeStamp}-fail.json`
+                        filename: `fail-${timeStamp}.json`,
+                        path: `./result/fail-${timeStamp}.json`
                     }, {
-                        filename: `${timeStamp}-success.json`,
-                        path: `./result/${timeStamp}-success.json`
+                        filename: `success-${timeStamp}.json`,
+                        path: `./result/success-${timeStamp}.json`
                     }
                 ]);
             }
